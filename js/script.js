1ad332da3b6815f4975187da92267b4726f6ae26
Rabin Gharti Magar <rabin_mgr@live.com>
2020-12-10 01:08:40 +0000

    new file added

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Plugin settings
+baguetteBox.run('.flex-container', {
+  animation: 'fadeIn',
+  // fullScreen: true,
+  buttons: true,
+});
+
+// Search Function
+$(document).ready(function () {
+  // Setting title attribute of <a> tag
+  $('.photo').attr('title', 'Click here to enlarge');
+
+  $('#search').on('keyup', function () {
+    let photo = $('.photo');
+    // Save user input & display it on a console
+    let search = $(this).val().toLowerCase();
+    // (for loop) to loop through all <a> tags
+    for (let i = 0; i < photo.length; i++) {
+      const caption = $('.photo')[i].getAttribute('data-caption').toLowerCase();
+      // Conditional statement
+      if (caption.includes(search)) {
+        $('.photo')[i].style.display = 'block';
+      } else {
+        $('.photo')[i].style.display = 'none';
+      }
+    }
+  });
+});
